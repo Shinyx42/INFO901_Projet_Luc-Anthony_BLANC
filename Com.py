177639ce83f3afd2416db06b8a3d4b76c49335b3
Shinyx42 <93926318+Shinyx42@users.noreply.github.com>
2024-09-18 14:14:05 +0200

    First running commit
    base du projet

---
+++ Com.py
@@ -0,0 +1,6 @@
+
+
+class Com():
+    def __init__(self):
+        print("init com")
+        
---
+++ Messages.py
@@ -0,0 +1,42 @@
+class Message():
+    def __init__(self, message, estampille):
+        self.message=message
+        self.estampille=estampille
+
+    def getMessage(self):
+        return self.message
+
+    def getEstampille(self):
+        return self.estampille
+
+class BroadcastMessage(Message):
+    def __init__(self, message, estampille, sender):
+        super().__init__(message, estampille)
+        self.sender=sender
+    
+    def isSender(self, sender):
+        return self.sender == sender
+    
+    def getSender(self):
+        return self.sender
+        
+class MessageTo(BroadcastMessage):
+    def __init__(self, message, estampille, sender, reciever):
+        super().__init__(message, estampille, sender)
+        self.reciever=reciever
+        
+    def isReciever(self, reciever):
+        return self.reciever == reciever
+    
+    def getReciever(self):
+        return self.reciever
+
+class Token():
+    def __init__(self, nextId):
+        self.nextId=nextId
+    def haveToken(self, myId):
+        return myId==self.nextId
+        
+class MessageSync(Message):
+    def __init__(self,estampille):
+        super().__init__("sync", estampille)
